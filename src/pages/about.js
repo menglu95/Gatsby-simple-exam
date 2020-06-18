1b5406d0e28f02d2e914febb09894ebc0467122e
LuMeng9 <LuMeng9@outlook.com>
2020-06-18 20:10:36 +0800

    2nd commit

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Header from "./components/header"
+
+export default function About() {
+  return (
+    <div style={{ color: `teal` }}>
+      <h1>About Gatsby</h1>
+      <Header />
+      <p>Such wow. Very React</p>
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ function Home() {
     <div style={{ color: `purple` }}>
       <h1>Hello, Gatsby</h1>
       <p>what is the gatsby</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
     </div>
   )
 }
